1757c996e7e19147733a2ae725be5b06b2f5f36d
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-11-23 14:27:24 +0800

    update profile info controller

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -12,8 +12,6 @@ exports.setupProfile = async (req, res) => {
     summary,
     skills,
     experience,
-    currentlyWorking,
-    achievements,
     projects,
     education,
     certification,
@@ -59,7 +57,14 @@ exports.setupProfile = async (req, res) => {
 
     // Experience
     for (const exp of experience) {
-      const { job_title, company_name, start_date, end_date } = exp;
+      const {
+        job_title,
+        company_name,
+        start_date,
+        end_date,
+        currentlyWorking,
+        achievements,
+      } = exp;
 
       await pool.query(
         "INSERT INTO experience(user_id, job_title, company_name, start_date, end_date, currently_working, achievements) VALUES($1, $2, $3, $4, $5, $6, $7)",
@@ -118,7 +123,7 @@ exports.getProfileInfo = async (req, res) => {
 
   try {
     const personalInfo = await pool.query(
-      "SELECT (full_name, email, phone, address, linkedin, github, portfolio, summary) FROM user_profile WHERE id = $1",
+      "SELECT full_name, email, phone, address, linkedin, github, portfolio, summary FROM user_profiles WHERE id = $1",
       [userId]
     );
 
@@ -179,16 +184,130 @@ exports.updateProfile = async (req, res) => {
     summary,
     skills,
     experience,
-    currentlyWorking,
-    achievements,
     projects,
     education,
     certification,
   } = req.body;
   const userId = req.user.id;
 
+  const client = await pool.connect();
+
   try {
+    await pool.query("BEIGN");
+
+    //user profile
+    await client.query("DELETE FROM user_profiles WHERE id = $1", [userId]);
+
+    await client.query(
+      `INSERT INTO user_profiles
+     (id, full_name, email, phone, address, linkedin, github, portfolio, summary)
+     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+     ON CONFLICT (id) DO UPDATE SET
+     phone = EXCLUDED.phone,
+     address = EXCLUDED.address,
+     linkedin = EXCLUDED.linkedin,
+     github = EXCLUDED.github,
+     portfolio = EXCLUDED.portfolio,
+     summary = EXCLUDED.summary,
+     updated_at = NOW()
+     RETURNING *`,
+      [
+        userId,
+        fullName,
+        email,
+        phone,
+        address,
+        linkedin,
+        github,
+        portfolio,
+        summary,
+      ]
+    );
+
+    // Skills
+    await client.query("DELETE FROM skills WHERE user_id = $1", [userId]);
+
+    for (const skill of skills) {
+      const { name } = skill;
+
+      await client.query(
+        "INSERT INTO skills(user_id, skill_name) VALUES($1, $2)",
+        [userId, name]
+      );
+    }
+
+    // Experience
+    await client.query("DELETE FROM experience WHERE user_id = $1", [userId]);
+
+    for (const exp of experience) {
+      const {
+        job_title,
+        company_name,
+        currentlyWorking,
+        achievements,
+        start_date,
+        end_date,
+      } = exp;
+
+      await client.query(
+        "INSERT INTO experience(user_id, job_title, company_name, start_date, end_date, currently_working, achievements) VALUES($1, $2, $3, $4, $5, $6, $7)",
+        [
+          userId,
+          job_title,
+          company_name,
+          start_date,
+          end_date,
+          currentlyWorking,
+          achievements,
+        ]
+      );
+    }
+
+    // Projects
+    await client.query("DELETE FROM projects WHERE user_id = $1", [userId]);
+
+    for (const project of projects) {
+      const { project_title, description, tech_stack, github_link, live_link } =
+        project;
+
+      await client.query(
+        "INSERT INTO projects(user_id, project_title, description, tech_stack, github_link, live_link) VALUES($1, $2, $3, $4, $5, $6)",
+        [userId, project_title, description, tech_stack, github_link, live_link]
+      );
+    }
+
+    //Education
+    await client.query("DELETE FROM education WHERE user_id = $1", [userId]);
+
+    for (const edu of education) {
+      const { degree, school_name, graduation_year } = edu;
+
+      await client.query(
+        "INSERT INTO education(user_id, degree, school_name, graduation_year) VALUES($1, $2, $3, $4)",
+        [userId, degree, school_name, graduation_year]
+      );
+    }
+
+    // Certificates
+    await client.query("DELETE FROM certificates WHERE user_id = $1", [userId]);
+
+    for (const certificate of certification) {
+      const { title, issuer, issue_date } = certificate;
+
+      await client.query(
+        "INSERT INTO certificates(user_id, title, issuer, issue_date) VALUES($1, $2, $3, $4)",
+        [userId, title, issuer, issue_date]
+      );
+    }
+
+    await client.query("COMMIT");
+
+    res.status(201).json({ message: "Updated successfully" });
   } catch (err) {
+    await client.query("ROLLBACK");
     console.log(err);
+    return res.status(500).json({ message: "Server error" });
+  } finally {
+    client.release();
   }
 };
